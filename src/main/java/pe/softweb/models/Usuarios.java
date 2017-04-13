882d9b70d03d5b949824cd50733153c7b3ba97d3
Pepe Valdivia <jvaldivia@softweb.pe>
2017-04-13 11:00:50 -0500

    Antes de probar JPA

---
+++ src/main/java/pe/softweb/models/Usuarios.java
@@ -0,0 +1,9 @@
+package pe.softweb.models;
+
+public class Usuarios {
+    private int id;
+    private String usuario;
+    private String contrasenia;
+    private String correo;
+    private int estadoUsuarioId;
+}
